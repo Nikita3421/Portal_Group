6608ff1faaab3cfa2763286911e2ec3d75c2d3fd
My Name <Me@example.com>
2024-06-09 12:25:28 +0200

    urls

---
+++ auth_sys/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from auth_sys import views
+
+# urlpatterns = [
+#     path('', views.TaskListView.as_view(), name='main'),
+#     path("login/", views.CustomLoginView.as_view(), name="login"),
+#     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+#     path("register/", views.RegisterView.as_view(), name="register"),
+# ]
+
+app_name = 'auth_sys'
