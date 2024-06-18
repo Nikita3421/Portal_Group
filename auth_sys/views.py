638bc5ec34a987d31d0db7006b41ec6cd41a7cff
Filip-Mistyuk <mistukfilipp@gmail.com>
2024-06-18 10:27:58 -0700

    Yo

--- auth_sys/views.py
+++ auth_sys/views.py
@@ -33,3 +33,44 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'Вы успешно вышли из системы!')
     return redirect(reverse_lazy("auth_sys:login"))
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse_lazy("auth_sys:login"))
+
+# портфолио + его главная страница
+class PortfolioDetailView(DetailView):
+    model = models.Portfolio
+    template_name = "portfolio/portfolio.html"
+    context_object_name = "portfolio"
+
+class PortfolioCreateView(CreateView):
+    model = models.Portfolio
+    form_class = PortfolioForm
+    template_name = "portfolio/portfolio_form.html"    
+    success_url = reverse_lazy('auth_sys:portfolio_main')
+
+class PortfolioMainDetailView(DetailView):
+    model = models.Portfolio
+    template_name = "portfolio/portfolio_main.html"
+    context_object_name = "portfolio"
+
+# проэкты портфолио
+
+class ProjectsInformationView(ListView):
+    model = models.PortfolioProjects
+    template_name = 'portfolio/projects_list.html'
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(portfolio=self.portfolio)
+    
+    def dispatch(self, request: HttpRequest, pk, *args: reverse_lazy, **kwargs: reverse_lazy) -> HttpResponse:
+        self.portfolio = get_object_or_404(models.Portfolio, id = pk )
+        return super().dispatch(request, *args, **kwargs)
+
+class ProjectsCreateView(CreateView):
+    model = models.PortfolioProjects
+    form_class = ProjectsForm
+    template_name = "portfolio/projects_form.html"    
+    success_url = reverse_lazy('auth_sys:portfolio_main')
