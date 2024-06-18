7580d1989d5383462a24ddb98e94db6bf942613e
My Name <Me@example.com>
2024-06-18 18:29:35 +0200

    portfolio

--- auth_sys/urls.py
+++ auth_sys/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.SignUpView.as_view(), name="signup"),
+    path('portfolio_main/<int:pk>/', views.PortfolioMainDetailView.as_view(), name="portfolio_main"),
+    path('portfolio_create/', views.PortfolioCreateView.as_view(), name='portfolio_create'),
+    path('portfolio_detail/<int:pk>/',views.PortfolioDetailView.as_view(), name="portfolio_detail"),
+    path('projects_list/<int:pk>/', views.ProjectsInformationView.as_view(), name="projects_list"),
+    path('projects_create/', views.ProjectsCreateView.as_view(), name="projects_create"),
 ]
 
 app_name = "auth_sys"
--- auth_sys/views.py
+++ auth_sys/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from auth_sys.forms import SignUpForm, LoginForm, UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, View, ListView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login, logout
-from django.urls import reverse_lazy
+from auth_sys.forms import PortfolioForm , ProjectsForm
+from auth_sys import models
+from django.contrib.auth.forms import UserCreationForm
 
 class SignUpView(CreateView):
     template_name = "auth_sys/signup.html"
@@ -26,3 +32,40 @@ class LoginView(LoginView):
 def logout_view(request):
     logout(request)
     return redirect(reverse_lazy("auth_sys:login"))
+
+# портфолио + его главная страница
+class PortfolioDetailView(DetailView):
+    model = models.Portfolio
+    template_name = "portfolio/portfolio.html"
+    context_object_name = "portfolio"
+
+class PortfolioCreateView(CreateView):
+    model = models.Portfolio
+    form_class = PortfolioForm
+    template_name = "portfolio/portfolio_form.html"    
+    success_url = reverse_lazy('auth_sys:portfolio_main')
+
+class PortfolioMainDetailView(DetailView):
+    model = models.Portfolio
+    template_name = "portfolio/portfolio_main.html"
+    context_object_name = "portfolio"
+
+# проэкты портфолио
+
+class ProjectsInformationView(ListView):
+    model = models.PortfolioProjects
+    template_name = 'portfolio/projects_list.html'
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(portfolio=self.portfolio)
+    
+    def dispatch(self, request: HttpRequest, pk, *args: reverse_lazy, **kwargs: reverse_lazy) -> HttpResponse:
+        self.portfolio = get_object_or_404(models.Portfolio, id = pk )
+        return super().dispatch(request, *args, **kwargs)
+
+class ProjectsCreateView(CreateView):
+    model = models.PortfolioProjects
+    form_class = ProjectsForm
+    template_name = "portfolio/projects_form.html"    
+    success_url = reverse_lazy('auth_sys:portfolio_main')
