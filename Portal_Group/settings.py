cf07f4bd09fd3537a237112dcce5cd4f9237407a
Filip-Mistyuk <mistukfilipp@gmail.com>
2024-06-18 19:02:50 -0700

    Vse rabotaet nice

--- Portal_Group/settings.py
+++ Portal_Group/settings.py
@@ -140,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'main:home'
