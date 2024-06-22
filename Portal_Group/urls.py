fc7f4a2b2486615da925e70dd533ec94ac8d800e
Vadym Hotko <poipooo600@gmail.com>
2024-06-23 00:34:10 +0100

    added survey models + mvt + crud

--- Portal_Group/urls.py
+++ Portal_Group/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/',include('forum.urls')),
+    path('survey/',include('survey.urls')),
 ]
 
 urlpatterns += static(
