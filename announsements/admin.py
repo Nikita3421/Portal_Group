1e64ba609ae5864ebdf2a9702285ffd6b81f822e
rusplane01 <rusplane01@gmail.com>
2024-06-22 15:54:58 +0300

    added news app and likes

---
+++ announsements/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from announsements.models import Announsement, Like
+
+# Register your models here.
+
+admin.site.register(Announsement)
+admin.site.register(Like)
---
+++ announsements/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Announsement(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    creation_date = models.DateTimeField()
+    media = models.ImageField(upload_to="images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('announsements:news_list')
+
+
+class Like(models.Model):
+    announsement = models.ForeignKey(Announsement, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_Announsements')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('announsement', 'user')  # Забезпечує унікальність лайків
