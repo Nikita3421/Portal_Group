9c7fc2d4ca8432be5facd2e859d0398d09775631
rusplane01 <rusplane01@gmail.com>
2024-06-22 16:57:46 +0300

    some changes added

--- Portal_Group/urls.py
+++ Portal_Group/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('forum/',include('forum.urls')),
     path('', include('main.urls')),
     path('auth_sys/', include('auth_sys.urls')),
-    path('portfolio/', include('portfolio.urls'))
+    path('portfolio/', include('portfolio.urls')),
     path('announsements/', include('announsements.urls')),
 ]
 
