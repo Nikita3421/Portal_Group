4282e13ecb88e58a5a03cc2d033b4ec5f9435f05
Vadym Hotko <poipooo600@gmail.com>
2024-06-27 00:48:38 +0100

    added thread crud

--- forum/models.py
+++ forum/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
 
 
 # Create your models here.
@@ -13,6 +14,10 @@ class Thread(models.Model):
     )
     title = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    
+    def get_absolute_url(self):
+        return reverse("forum:thread-detail", kwargs={"pk": self.pk})
+    
     
     def __str__(self):
         return self.title
