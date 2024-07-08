0bea9c0b95d7f331ca042d807ce68bb6ced98182
rusplane01 <rusplane01@gmail.com>
2024-07-08 23:53:34 +0300

    grades done

--- grades/urls.py
+++ grades/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GradesListView, GradeCreateView
+from .views import GradesListView, GradeCreateView, GradeDeleteView
 
 urlpatterns = [
     path('grades_list', GradesListView.as_view(), name='grades_list'),
-    path('grade_create', GradeCreateView.as_view(), name='grade_create')
+    path('grade_create', GradeCreateView.as_view(), name='grade_create'),
+    path("grade/delete/<int:pk>/", GradeDeleteView.as_view(), name="grade_delete"),
 ]
 
 app_name = 'grades'
