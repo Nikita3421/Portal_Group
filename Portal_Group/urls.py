d27f8ac424ae38c2a05b096a3287513f5ba3a335
Vadym Hotko <poipooo600@gmail.com>
2024-07-13 00:25:59 +0100

    added internationalization

--- Portal_Group/urls.py
+++ Portal_Group/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('forum/',include('forum.urls')),
     path('survey/',include('survey.urls')),
@@ -33,7 +34,8 @@ urlpatterns = [
     path('grades/', include('grades.urls')),
     path('friends/', include('friends.urls')),
     path('profile/', include('pro_file.urls')),
-]
+    path('rosetta/', include('rosetta.urls')),
+)
 
 urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
