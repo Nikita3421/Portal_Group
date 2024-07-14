4a2b7b19c8b91d555c614a86165bc77208fd9fe4
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-07-14 03:20:43 +0300

    changed thread_list.html

--- forum/views.py
+++ forum/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 
 from .mixins import PermissionOrOwnerRequiredMixin
 from . import models
-from .forms import OptionFormSet
+from .forms import OptionFormSet, ThreadForm
 
 
 # Create your views here.
@@ -47,7 +47,7 @@ class ThreadPostListView(ListView):
     
 class ThreadCreateView(PermissionRequiredMixin,CreateView): 
     model = models.Thread
-    fields = ['title']
+    form_class = ThreadForm
     permission_required = 'forum.add_thread'
     
     def form_valid(self, form):
